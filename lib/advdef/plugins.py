f465497f55049f8c53e1a241222b82cfe0e34260
Justin Pallas <justin.pallas@student.htw-berlin.de>
2025-10-28 11:50:38 +0100

    switch to relative imports

--- lib/advdef/plugins.py
+++ lib/advdef/plugins.py
@@ -3,17 +3,17 @@
 from __future__ import annotations
 
 # Dataset builders
-from advdef.datasets import imagenet_autoattack  # noqa: F401
+from .datasets import imagenet_autoattack  # noqa: F401
 
 # Attacks
-from advdef.attacks import autoattack  # noqa: F401
+from .attacks import autoattack  # noqa: F401
 
 # Inference backends
-from advdef.inference import timm_backend  # noqa: F401
+from .inference import timm_backend  # noqa: F401
 
 # Evaluation helpers
-from advdef.evaluation import imagenet  # noqa: F401
+from .evaluation import imagenet  # noqa: F401
 
 # Defenses (wrappers point to external implementations)
-from advdef.defenses import jpeg  # noqa: F401
-from advdef.defenses import r_smoe  # noqa: F401
+from .defenses import jpeg  # noqa: F401
+from .defenses import r_smoe  # noqa: F401
